6c2affce1e5316bbf72799779d79889481cc6af2
Tharindu <tharindunirmal996@gmail.com>
2024-11-02 21:40:57 +0530

    nav bar configured

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styles from './Navbar.module.scss';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Navbar = () => {
+  const navRef = useRef();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle(styles.responsiveNav);
+  };
+
   return (
     <header className={`${styles.header}`}>
-      <a href='/' className={`${styles.logo}`}>
+      <a href='/' className={`${styles.header__logo}`}>
         Logo
       </a>
 
-      <nav className={`${styles.navbar}`}>
+      <nav ref={navRef} className={`${styles.header__navbar}`}>
         <a href='/'>Home</a>
         <a href='/'> About</a>
         <a href='/'> Contact Us</a>
+
+        <button
+          className={`${styles.header__navBtn} ${styles.header__navBtnClose}`}
+          onClick={showNavbar}
+        >
+          <FaTimes />
+        </button>
       </nav>
+
+      <button className={`${styles.header__navBtn}`} onClick={showNavbar}>
+        <FaBars />
+      </button>
     </header>
   );
 };
